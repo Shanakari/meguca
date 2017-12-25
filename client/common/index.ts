c3af7f9d55acc950ac94e30060380256f3018df4
Shanakari <34842124+Shanakari@users.noreply.github.com>
2017-12-25 23:20:02 +0100

    Update index.ts

--- client/common/index.ts
+++ client/common/index.ts
@@ -1,6 +1,5 @@
 // Common types and constants in a separate module to avoid circular
 // dependencies
-
 // Detect FireFox, so we can disable any functionality it's retarded bugs and
 // data races break
 export const isCuck = navigator.userAgent.toLowerCase().includes("firefox")
@@ -11,6 +10,7 @@ export type PostLink = [number, number]
 // Data of any post. In addition to server-sent JSON includes the state
 // property.
 export interface PostData {
+	editing: boolean
 	deleted: boolean
 	banned: boolean
 	sage: boolean
@@ -48,6 +48,58 @@ export type TextState = {
 const enum commandType { dice, flip, eightBall, syncWatch, pyu, pcount }
 
 // Single hash command result delivered from the server
+export interface Command {
+	type: commandType
+	val: any
+}
+
+// Data of an OP post
+export interface ThreadData extends PostData {
+	nonLive: boolean
+	postCtr: number
+	imageCtr: number
+	replyTime: number
+	bumpTime: number
+	subject: string
+	board: string
+	posts?: PostData[]
+}
+
+// Data of a board page
+export type BoardData = {
+	pages: number
+	threads: ThreadData[]
+}
+
+// Image data embeddable in posts and thread hashes
+export interface ImageData {
+	apng: boolean
+	audio: boolean
+	video: boolean
+	spoiler: boolean
+	fileType: fileTypes
+	thumbType: fileTypes
+	length?: number
+	artist?: string
+	title?: string
+	size: number
+	// [width, height, thumbnail_width, thumbnail_height]
+	dims: [number, number, number, number]
+	MD5: string
+	SHA1: string
+	name: string
+
+	// Added client-side
+	expanded: boolean           // Thumbnail is expanded
+	tallerThanViewport: boolean // Image is taller than the current viewport
+	revealed: boolean           // Revealing a hidden image with [Show]
+}
+
+// Possible file types of a post image
+export enum fileTypes {
+	jpg, png, gif, webm, pdf, svg, mp4, mp3, ogg, zip, "7z", "tar.gz", "tar.xz",
+	flac, noFile, txt
+}
 export interface Command {
 	type: commandType
 	val: any
