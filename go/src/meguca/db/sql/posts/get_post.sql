28505147226d7758b17399258db6d0788cb2c0fb
Shanakari <34842124+Shanakari@users.noreply.github.com>
2017-12-26 00:02:46 +0100

    Update get_post.sql

--- go/src/meguca/db/sql/posts/get_post.sql
+++ go/src/meguca/db/sql/posts/get_post.sql
@@ -1,7 +1,7 @@
-select op, board, banned, spoiler, deleted, sage, id, time, body, flag,
+select op, board, editing, banned, spoiler, deleted, sage, id, time, body, flag,
 		name, trip, auth, links, commands, imageName, posterID,
 		images.*
 	from posts
 	left outer join images
 		on posts.SHA1 = images.SHA1
-	where id = $1
+where id = $1
